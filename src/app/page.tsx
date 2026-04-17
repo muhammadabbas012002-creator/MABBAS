63e311f235e81d594dc9fb866da1cd188c8bfe88
muhammadabbas012002-creator <muhammad.abbas.012002@gmail.com>
2026-04-17 14:16:13 +0000

    update image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,19 +22,60 @@ export default function Home() {
             <ContactSection />
             <ContactSection />
 
-<div style={{ textAlign: "center", padding: "40px" }}>
-  <h2>Founder</h2>
-  <img src="/founder.jpg" width="150" style={{ borderRadius: "50%" }} />
-  <h3>Muhammad Abbas</h3>
+<div style={{ textAlign: "center", padding: "60px 20px" }}>
+  <h2 style={{ fontSize: "28px", marginBottom: "20px" }}>Founder</h2>
 
-  <h2 style={{ marginTop: "30px" }}>Our Team</h2>
+  <img
+    src="/founder.jpg"
+    style={{
+      width: "150px",
+      height: "150px",
+      borderRadius: "50%",
+      objectFit: "cover",
+      display: "block",
+      margin: "0 auto"
+    }}
+  />
 
-  <img src="/1.jpg" width="120" style={{ borderRadius: "50%" }} />
-  <p>Team Member 1</p>
+  <h3 style={{ marginTop: "15px" }}>Muhammad Abbas</h3>
 
-  <img src="/2.jpg" width="120" style={{ borderRadius: "50%" }} />
-  <p>Team Member 2</p>
-</div>
+  <h2 style={{ marginTop: "50px", fontSize: "26px" }}>Our Team</h2>
+
+  <div style={{
+    display: "flex",
+    justifyContent: "center",
+    gap: "40px",
+    marginTop: "30px",
+    flexWrap: "wrap"
+  }}>
+
+    <div style={{ textAlign: "center" }}>
+      <img
+        src="/1.jpg"
+        style={{
+          width: "120px",
+          height: "120px",
+          borderRadius: "50%",
+          objectFit: "cover"
+        }}
+      />
+      <p>Team Member 1</p>
+    </div>
+
+    <div style={{ textAlign: "center" }}>
+      <img
+        src="/2.jpg"
+        style={{
+          width: "120px",
+          height: "120px",
+          borderRadius: "50%",
+          objectFit: "cover"
+        }}
+      />
+      <p>Team Member 2</p>
+    </div>
+
+  </div>
 
 </main>
     
